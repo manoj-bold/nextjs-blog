016cf533b719e6c7e129a399362d4f1312004959
Manoj Mishra <manoj.mishra@bold.com>
2024-04-18 02:15:21 +0530

    Add Post Grid & Post Items

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,36 +1,13 @@
 import Link from "next/link";
+
+import PostsGrid from "../posts/posts-grid";
 import classes from "./featured-posts.module.css";
 
-export default function FeaturedPosts() {
+export default function FeaturedPosts({ posts }) {
   return (
     <section className={classes.latest}>
       <h2>Featured Posts</h2>
-      <ul>
-        <li>
-          <Link href="/posts/a-new-beginning">
-            <h3>A New Beginning</h3>
-            <p>Starting something new is always exciting!</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/posts/the-time-is-now">
-            <h3>The Time is Now</h3>
-            <p>Preparing for the big journey ahead.</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/posts/what-you-need-to-know-about-css-variables">
-            <h3>What You Need to Know About CSS Variables</h3>
-            <p>Let's have a closer look at CSS variables.</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/posts/next-js-features-you-need-to-know">
-            <h3>Next.js Features You Need to Know</h3>
-            <p>Learn about the most important Next.js features!</p>
-          </Link>
-        </li>
-      </ul>
+      <PostsGrid posts={posts} />
     </section>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,48 @@ import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 
 export default function HomePage() {
+  const DUMMY_POSTS = [
+    {
+      slug: "getting-started-with-nextjs",
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework that provides a great developer experience and many awesome features. In this article, you'll learn how to create a new NextJS project and deploy it on Vercel.",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs",
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework that provides a great developer experience and many awesome features. In this article, you'll learn how to create a new NextJS project and deploy it on Vercel.",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs",
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework that provides a great developer experience and many awesome features. In this article, you'll learn how to create a new NextJS project and deploy it on Vercel.",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs",
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework that provides a great developer experience and many awesome features. In this article, you'll learn how to create a new NextJS project and deploy it on Vercel.",
+      date: "2022-02-10",
+    },
+  ];
+
   return (
-    <div>
+    <>
       <Head>
         <title>Home</title>
       </Head>
       <Hero />
-      <FeaturedPosts />
-    </div>
+      <FeaturedPosts posts={DUMMY_POSTS} />
+    </>
   );
 }
