a638450c47d340801cce877afe3656ae8db65246
Manoj Mishra <manoj.mishra@bold.com>
2024-04-24 08:48:48 +0530

    Add All post detail component

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,15 @@
+import classes from "./post-content.module.css";
+import PostHeader from "./post-header";
+
+export default function PostContent(props) {
+  const { post } = props;
+
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <p>{post.content}</p>
+    </article>
+  );
+}
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,12 +1,41 @@
 import Head from "next/head";
+import PostContent from "../../components/posts/post-detail/post-content";
 
-export default function PostPage() {
+export default function PostDetailPage({ post }) {
   return (
-    <div>
+    <>
       <Head>
-        <title>Post</title>
+        <title>{post.title}</title>
+        <meta name="description" content={post.excerpt} />
       </Head>
-      <h1>Post</h1>
-    </div>
+      <PostContent post={post} />
+    </>
   );
+}
+
+export async function getStaticProps() {
+  const post = {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    date: "2022-02-10",
+    content: "# This is a first post",
+  };
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 600,
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { slug: "getting-started-with-nextjs" } },
+      { params: { slug: "getting-started-with-react" } },
+    ],
+    fallback: false,
+  };
 }
