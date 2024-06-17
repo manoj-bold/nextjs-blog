888f21702e5e519d770bfe234757ffaab338da60
Manoj Mishra <manoj.mishra@bold.com>
2024-06-17 23:58:10 +0530

    Add contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,5 @@
-import Head from "next/head";
+import ContactForm from "../components/contact/contact-form";
 
 export default function ContactPage() {
-  return (
-    <div>
-      <Head>
-        <title>Contact</title>
-      </Head>
-      <h1>Contact</h1>
-    </div>
-  );
+  return <ContactForm />;
 }
